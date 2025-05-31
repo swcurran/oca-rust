65fd944b55aa872b7f27e9f5fd17195074b4b7d4
Robert Mitwicki <robert.mitwicki@humancolossus.org>
2025-05-31 12:13:39 +0200

    WIP: rename oca to oca-store

--- oca-store/src/lib.rs
+++ oca-store/src/lib.rs
@@ -2,6 +2,5 @@ pub mod data_storage;
 pub mod facade;
 pub mod repositories;
 pub use facade::Facade;
-pub use oca_bundle::{Encode as EncodeBundle, HashFunctionCode, SerializationFormats};
 #[cfg(feature = "local-references")]
 pub(crate) mod local_references;
